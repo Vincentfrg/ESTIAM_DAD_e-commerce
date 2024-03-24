4f92c98cc2a6396de7158b5b8098432c4a0952d1
Jonathan_mqhm <137342185+FireFox-d3vFR@users.noreply.github.com>
2024-03-24 19:55:55 +0100

    Update : Créer un espace pour Dashboard Admin

---
+++ laravel-vue-ecommerce/backend/src/components/AppLayout.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div class="max-w-md w-full space-y-8">
+            <div>
+                Partie pour le Layout
+            </div>
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const { title } = defineProps({
+    title: String
+});
+
+// import { LockClosedIcon } from "@heroicons/vue/solid";
+</script>
+
+<style scoped></style>
--- laravel-vue-ecommerce/backend/src/router/index.js
+++ laravel-vue-ecommerce/backend/src/router/index.js
@@ -1,14 +1,22 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Dashboard from "../views/Dashboard.vue";
+import AppLayout from "../components/AppLayout.vue";
 import Login from "../views/Login.vue";
+import Dashboard from "../views/Dashboard.vue";
 import RequestPassword from "../views/RequestPasswordReset.vue";
 import ResetPassword from "../views/ResetPassword.vue";
 
 const routes = [
     {
-        path: "/dashboard",
-        name: "dashboard",
-        component: Dashboard,
+        path: "/app",
+        name: "app",
+        component: AppLayout,
+        children: [
+            {
+                path: "dashboard",
+                name: "app.dashboard",
+                component: Dashboard,
+            },
+        ]
     },
     {
         path: "/login",
