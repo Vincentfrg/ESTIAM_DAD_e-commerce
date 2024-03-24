54af08aa834e886c3228811752477d5eb11b4a24
Jonathan_mqhm <137342185+FireFox-d3vFR@users.noreply.github.com>
2024-03-24 10:16:22 +0100

    Update : installation Vuex et création "Store"

--- laravel-vue-ecommerce/backend/src/main.js
+++ laravel-vue-ecommerce/backend/src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
+import store from './store'
 import './index.css'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(store)
+    .mount('#app')
