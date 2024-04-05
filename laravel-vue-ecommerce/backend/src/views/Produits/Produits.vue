077a879e77432f9ae61864b020d768d580a5a196
Jonathan_mqhm <137342185+FireFox-d3vFR@users.noreply.github.com>
2024-04-05 16:20:29 +0200

    Update : Message (button click)

--- laravel-vue-ecommerce/backend/src/views/Produits/Produits.vue
+++ laravel-vue-ecommerce/backend/src/views/Produits/Produits.vue
@@ -1,16 +1,27 @@
 <template>
     <div class="flex items-center justify-between mb-3">
         <h1 class="text-3xl font-semibold">Produits</h1>
-        <button type="submit"
+        <button
+            type="submit"
+            @click="showProduitModel"
             class="flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             Ajouter un nouveau Produit
         </button>
     </div>
+    <ProduitModel v-model="showModal"/>
     <ProduitsTableau/>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+import ProduitModel from './ProduitModel.vue';
 import ProduitsTableau from './ProduitsTableau.vue';
+
+const showModal = ref(false);
+
+function showProduitModel() {
+    showModal.value = true;
+}
 </script>
 
 
