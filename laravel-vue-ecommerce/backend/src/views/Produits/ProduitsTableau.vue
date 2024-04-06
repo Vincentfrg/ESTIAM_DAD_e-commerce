bbac87e1a8bcb6f9ede92903bbbb779e1b0cfadd
Jonathan_mqhm <137342185+FireFox-d3vFR@users.noreply.github.com>
2024-04-06 22:10:01 +0200

    Update : Animation - Liste de produits (Tableau)

--- laravel-vue-ecommerce/backend/src/views/Produits/ProduitsTableau.vue
+++ laravel-vue-ecommerce/backend/src/views/Produits/ProduitsTableau.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-white p-4 rounded-lg shadow">
+    <div class="bg-white p-4 rounded-lg shadow animate-fade-in-down">
         <div class="flex justify-between border-b-2 pb-3">
             <div class="flex items-center">
                 <span class="whitespace-nowrap mr-3">Produits/Page :</span>
@@ -45,7 +45,7 @@
                 </tr>
             </tbody>
             <tbody v-else>
-                <tr v-for="product of products.data">
+                <tr v-for="(product, index) of products.data" class="animate-fade-in-down" :style="{'animation-delay': `${index * 0.075}s`}">
                     <td class="border-b p-2">{{ product.id }}</td>
                     <td class="border-b p-2">
                         <img class="w-16" :src="product.image_url" :alt="product.title">
