94d60076ab21353f2761197521c98ced4389d0c1
Jonathan_mqhm <137342185+FireFox-d3vFR@users.noreply.github.com>
2024-04-07 22:56:07 +0200

    Update : Installation du site e-commerce (connexion/déconnexion).

--- laravel-vue-ecommerce/resources/views/layouts/navigation.blade.php
+++ laravel-vue-ecommerce/resources/views/layouts/navigation.blade.php
@@ -13,29 +13,6 @@
         <ul>
           <li>
             <a
-              href="/src/index.html"
-              class="block py-2 px-3 transition-colors hover:bg-slate-800"
-              >Accueil</a
-            >
-          </li>
-          <li>
-            <a
-              href="#"
-              class="block py-2 px-3 transition-colors hover:bg-slate-800"
-              >Catégories</a
-            >
-          </li>
-          <li>
-            <a
-              href="#"
-              class="block py-2 px-3 transition-colors hover:bg-slate-800"
-              >Tendances</a
-            >
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <a
               href="/src/cart.html"
               class="relative flex items-center justify-between py-2 px-3 transition-colors hover:bg-slate-800"
             >
@@ -66,51 +43,13 @@
               <!--/ Cart Items Counter -->
             </a>
           </li>
-          <li x-data="{open: false}" class="relative">
-            <a
-              @click="open = !open"
-              class="cursor-pointer flex justify-between items-center py-2 px-3 hover:bg-slate-800"
-            >
-              <span class="flex items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-5 w-5 mr-2"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  stroke-width="2"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                  />
-                </svg>
-                Votre Compte
-              </span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5"
-                viewBox="0 0 20 20"
-                fill="currentColor"
+          @if (!Auth::guest())
+            <li x-data="{open: false}" class="relative">
+              <a
+                @click="open = !open"
+                class="cursor-pointer flex justify-between items-center py-2 px-3 hover:bg-slate-800"
               >
-                <path
-                  fill-rule="evenodd"
-                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </a>
-            <ul
-              x-show="open"
-              x-transition
-              class="z-10 right-0 bg-slate-800 py-2"
-            >
-              <li>
-                <a
-                  href="/src/profile.html"
-                  class="flex px-3 py-2 hover:bg-slate-900"
-                >
+                <span class="flex items-center">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     class="h-5 w-5 mr-2"
@@ -125,142 +64,158 @@
                       d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
                     />
                   </svg>
-                  Votre profil
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/src/watchlist.html"
-                  class="flex items-center px-3 py-2 hover:bg-slate-900"
+                  Votre Compte
+                </span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="h-5 w-5 mr-2"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-width="2"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                    />
-                  </svg>
-                  Liste d'Achats
-
-                  <small
-                    x-show="$store.header.watchlistItems"
-                    x-transition
-                    x-text="$store.header.watchlistItems"
-                    class="py-[2px] px-[8px] rounded-full bg-red-500"
-                  ></small>
-                </a>
-              </li>
-              <li class="hover:bg-slate-900">
-                <a
-                  href="/src/orders.html"
-                  class="flex items-center px-3 py-2 hover:bg-slate-900"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="h-5 w-5 mr-2"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-width="2"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
-                    />
-                  </svg>
-                  Vos Commandes
-                </a>
-              </li>
-              <li class="hover:bg-slate-900">
-                <a
-                  href="/src/logout.html"
-                  class="flex items-center px-3 py-2 hover:bg-slate-900"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="h-5 w-5 mr-2"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-width="2"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-                    />
-                  </svg>
-                  Déconnexion
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a
-              href="/src/login.html"
-              class="flex items-center py-2 px-3 transition-colors hover:bg-slate-800"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6 mr-2"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
+                  <path
+                    fill-rule="evenodd"
+                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </a>
+              <ul
+                x-show="open"
+                x-transition
+                class="z-10 right-0 bg-slate-800 py-2"
               >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
-                />
-              </svg>
-              Connexion
-            </a>
-          </li>
-          <li class="px-3 py-3">
-            <a
-              href="/src/signup.html"
-              class="block text-center text-white bg-emerald-600 py-2 px-3 rounded shadow-md hover:bg-emerald-700 active:bg-emerald-800 transition-colors w-full"
-            >
-              Inscription
-            </a>
-          </li>
+                <li>
+                  <a
+                    href="/src/profile.html"
+                    class="flex px-3 py-2 hover:bg-slate-900"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-5 w-5 mr-2"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                      />
+                    </svg>
+                    Votre profil
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/src/watchlist.html"
+                    class="flex items-center px-3 py-2 hover:bg-slate-900"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-5 w-5 mr-2"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                      />
+                    </svg>
+                    Liste d'Achats
+
+                    <small
+                      x-show="$store.header.watchlistItems"
+                      x-transition
+                      x-text="$store.header.watchlistItems"
+                      class="py-[2px] px-[8px] rounded-full bg-red-500"
+                    ></small>
+                  </a>
+                </li>
+                <li class="hover:bg-slate-900">
+                  <a
+                    href="/src/orders.html"
+                    class="flex items-center px-3 py-2 hover:bg-slate-900"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-5 w-5 mr-2"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
+                      />
+                    </svg>
+                    Vos Commandes
+                  </a>
+                </li>
+                <li class="hover:bg-slate-900">
+                    <form action="{{ route('logout') }}" method="post">
+                        @csrf
+                        <button type="submit" class="flex w-full items-center px-3 py-2 hover:bg-slate-900">
+                            <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="h-5 w-5 mr-2"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            stroke-width="2"
+                        >
+                            <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                            />
+                        </svg>
+                        Déconnexion
+                        </button>
+                    </form>
+                </li>
+              </ul>
+            </li>
+          @else
+            <li>
+              <a
+                href="{{ route('login') }}"
+                class="flex items-center py-2 px-3 transition-colors hover:bg-slate-800"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6 mr-2"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
+                  />
+                </svg>
+                Connexion
+              </a>
+            </li>
+            <li class="px-3 py-3">
+              <a
+                href="{{ route('register') }}"
+                class="block text-center text-white bg-emerald-600 py-2 px-3 rounded shadow-md hover:bg-emerald-700 active:bg-emerald-800 transition-colors w-full"
+              >
+                Inscription
+              </a>
+            </li>
+          @endif
         </ul>
       </div>
       <!--/ Responsive Menu -->
-      <nav class="hidden md:block">
-        <ul class="grid grid-flow-col">
-          <li>
-            <a
-              href="/src/index.html"
-              class="block py-navbar-item px-navbar-item hover:bg-slate-900"
-              >Accueil</a
-            >
-          </li>
-          <li>
-            <a
-              href="#"
-              class="block py-navbar-item px-navbar-item hover:bg-slate-900"
-              >Catégories</a
-            >
-          </li>
-          <li>
-            <a
-              href="#"
-              class="block py-navbar-item px-navbar-item hover:bg-slate-900"
-              >Tendances</a
-            >
-          </li>
-        </ul>
-      </nav>
       <nav class="hidden md:block">
         <ul class="grid grid-flow-col items-center">
           <li>
@@ -292,6 +247,7 @@
               ></small>
             </a>
           </li>
+          @if (!Auth::guest())
           <li x-data="{open: false}" class="relative">
             <a
               @click="open = !open"
@@ -410,59 +366,61 @@
                 </a>
               </li>
               <li>
+                <form action="{{ route('logout') }}" method="post">
+                    @csrf
+                    <button type="submit" class="flex w-full px-3 py-2 hover:bg-slate-900">
+                        <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-5 w-5 mr-2"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        stroke-width="2"
+                    >
+                        <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                        />
+                    </svg>
+                    Déconnexion
+                    </button>
+                </form>
+              </li>
+            </ul>
+          </li>
+          @else
+            <li>
                 <a
-                  href="/src/logout.html"
-                  class="flex px-3 py-2 hover:bg-slate-900"
+                href="{{ route('login') }}"
+                class="flex items-center py-navbar-item px-navbar-item hover:bg-slate-900"
                 >
-                  <svg
+                <svg
                     xmlns="http://www.w3.org/2000/svg"
                     class="h-5 w-5 mr-2"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
                     stroke-width="2"
-                  >
+                >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
                     />
-                  </svg>
-                  Déconnexion
+                </svg>
+                Connexion
                 </a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a
-              href="/src/login.html"
-              class="flex items-center py-navbar-item px-navbar-item hover:bg-slate-900"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5 mr-2"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
-                />
-              </svg>
-              Connexion
-            </a>
-          </li>
-          <li>
-            <a
-              href="/src/signup.html"
-              class="inline-flex items-center text-white bg-emerald-600 py-2 px-3 rounded shadow-md hover:bg-emerald-700 active:bg-emerald-800 transition-colors mx-5"
-            >
-              Inscription
-            </a>
-          </li>
+            </li>
+            <li>
+                <a
+                href="{{ route('register') }}"
+                class="inline-flex items-center text-white bg-emerald-600 py-2 px-3 rounded shadow-md hover:bg-emerald-700 active:bg-emerald-800 transition-colors mx-5"
+                >
+                Inscription
+                </a>
+            </li>
+          @endif
         </ul>
       </nav>
       <button
--- laravel-vue-ecommerce/routes/auth.php
+++ laravel-vue-ecommerce/routes/auth.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
