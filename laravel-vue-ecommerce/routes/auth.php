99f53f52f04c9d2fa2967d1780926a9e7f3adffc
Jonathan_mqhm <137342185+FireFox-d3vFR@users.noreply.github.com>
2024-04-07 23:00:52 +0200

    Update : Suppression d'une ligne.

--- laravel-vue-ecommerce/routes/auth.php
+++ laravel-vue-ecommerce/routes/auth.php
@@ -9,7 +9,6 @@ use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
