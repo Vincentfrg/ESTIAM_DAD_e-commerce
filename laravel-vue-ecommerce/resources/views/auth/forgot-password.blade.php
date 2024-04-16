2176a8f4a46ec6f34fafe1092d942e80a6933dcb
Jonathan_mqhm <137342185+FireFox-d3vFR@users.noreply.github.com>
2024-04-16 03:23:35 +0200

    Update : Créer un formulaire de réinitialisation de mot de passe.

--- laravel-vue-ecommerce/resources/views/auth/forgot-password.blade.php
+++ laravel-vue-ecommerce/resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,25 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
-
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
+<x-app-layout>
+    <form action="{{ route('password.email') }}" method="post" class="w-[400px] mx-auto p-6 my-16">
         @csrf
+        <h2 class="text-2xl font-semibold text-center mb-5">
+            Mot de passe oublié ? 🥺
+        </h2>
+        <p class="text-center text-gray-500 mb-6">
+            ou
+            <a href="{{route('login')}}" class="text-purple-600 hover:text-purple-500">retour à la page de connexion</a>.
+        </p>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+        <!-- Session Status -->
+        <x-auth-session-status class="mb-4" :status="session('status')" />
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+        <div class="mb-3">
+            <x-input id="loginEmail" type="email" name="email" :value="old('email')" placeholder="Votre adresse mail" required autofocus />
         </div>
+        <div class="mb-4 text-gray-600 dark:text-gray-400 text-xs text-center">
+            {{ __('Saisissez votre adresse e-mail et nous vous enverrons des instructions pour réinitialiser votre mot de passe.') }}
+        </div>
+        <button class="btn-primary bg-emerald-500 hover:bg-emerald-600 active:bg-emerald-700 w-full">
+            Continuer
+        </button>
     </form>
-</x-guest-layout>
+</x-app-layout>
