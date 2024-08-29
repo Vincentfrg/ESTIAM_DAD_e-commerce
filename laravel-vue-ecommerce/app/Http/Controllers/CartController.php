9e0a7610ba6691901d77e8de9888faf286f35a72
Vincentfrg <vferrag78@gmail.com>
2024-08-29 14:00:11 +0200

    card controller

--- laravel-vue-ecommerce/app/Http/Controllers/CartController.php
+++ laravel-vue-ecommerce/app/Http/Controllers/CartController.php
@@ -15,7 +15,7 @@ class CartController extends Controller
 {
     public function index()
     {
-        $cartItems = Cart::getCartItem();
+        $cartItems = Cart::getCartItems();
 
         $ids = Arr::pluck($cartItems, 'product_id');
         $products = Product::query()->whereIn('id', $ids)->get();
@@ -49,7 +49,7 @@ class CartController extends Controller
                 'count' => Cart::getCartItemsCount()
             ]);
         } else {
-            $cartItems = json_decode($request->cookie('cart_items'), [], true);
+            $cartItems = json_decode($request->cookie('cart_items'), '[]', true);
             $productFound = false;
             foreach ($cartItems as $item) {
                 if ($item['product_id'] === $product->id) {
@@ -67,7 +67,7 @@ class CartController extends Controller
                 ];
             }
             Cookie::queue('cart_items', json_encode($cartItems), 60 * 24 * 30);
-            return response(['count' => Cart::getCountFromCartItems($cartItems)]);
+            return response(['count' => Cart::getCountFromItems($cartItems)]);
         }
     }
 
@@ -92,7 +92,7 @@ class CartController extends Controller
                 }
             }
             Cookie::queue('cart_items', json_encode($cartItems), 60 * 24 * 30);
-            return response(['count' => Cart::getCountFromCartItems($cartItems)]);
+            return response(['count' => Cart::getCountFromItems($cartItems)]);
 
 
         }
@@ -119,7 +119,7 @@ class CartController extends Controller
                 } 
             }
             Cookie::queue('cart_items', json_encode($cartItems), 60 * 24 * 30);
-            return response(['count' => Cart::getCountFromCartItems($cartItems)]);
+            return response(['count' => Cart::getCountFromItems($cartItems)]);
         }
     }
 }
--- laravel-vue-ecommerce/app/Http/Controllers/Helpers/Cart.php
+++ laravel-vue-ecommerce/app/Http/Controllers/Helpers/Cart.php
@@ -1,5 +1,6 @@
 <?php
 
+namespace App\Http\Helpers;
 use App\Models\CartItem;
 
 
