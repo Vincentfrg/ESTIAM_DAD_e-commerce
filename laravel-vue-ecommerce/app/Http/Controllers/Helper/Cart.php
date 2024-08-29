15e0d201f9a2d2d235cbc4f0829b72980edf7e0a
Vincentfrg <vferrag78@gmail.com>
2024-08-29 12:16:20 +0200

    card helper with reusable method

---
+++ laravel-vue-ecommerce/app/Http/Controllers/Helper/Cart.php
@@ -0,0 +1,75 @@
+<?php
+
+use App\Models\CartItem;
+use function PHPSTORM_META\map;
+
+class Cart
+{
+    public static function getCartItemsCount(): int
+    {
+        $request = \request();
+        $user = $request->user();
+        if ($user) {
+            return CartItem::where('user_id', $user->id)->sum('quantity');
+        } else {
+            $cartItems = self::getCookieCartItems();
+
+            return array_reduce(
+                $cartItems,
+                fn($carry, $item) => $carry + $item['quantity'],
+                initial: 0
+            );
+        };
+    }
+
+    public static function getCartItems()
+    {
+        $request = \request();
+        $user = $request->user();
+        if ($user) {
+            return CartItem::where('user_id', $user->id)->get()->map(
+                fn($item) => ['product-id' => $item->product_id, 'quantity' => $item->quantity]
+            );
+        } else {
+            return self::getCookieCartItems();
+        }
+    }
+
+    public static function getCookieCartItems()
+    {
+        $request = \request();
+        return  json_decode($request->cookie('cart_items', ['']), true);
+    }
+
+    public static function getCountFromItems($cartItems)
+    {
+        return array_reduce(
+            $cartItems,
+            fn($carry, $items) => $carry + $items['quantity'],
+            initial: 0
+        );
+    }
+
+    public static function moveCartItemsIntoDb()
+    {
+        $request = \request();
+        $cartItems = self::getCookieCartItems();
+        $dbCartItems = CartItem::where(['user_id' => $request->user()->id])->get()->keyBy('product_id');
+        $newCartItems = [];
+        foreach ($cartItems as $cartItem) {
+            if (isset($dbCartItems[$cartItem['product_id']])) {
+                continue;
+            }
+            $newCartItems[] = [
+                'user_id' => $request->user()->id,
+                'product_id' => $cartItem['product_id'],
+                'quantity' => $cartItem['quantity'],
+
+            ];
+
+            if (!empty($newCartItems)) {
+                CartItem::insert($newCartItems);
+            }
+        }
+    }
+}
---
+++ laravel-vue-ecommerce/resources/views/cart/index.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+    <h1>
+        Mon panier
+    </h1>
+</x-app-layout>
