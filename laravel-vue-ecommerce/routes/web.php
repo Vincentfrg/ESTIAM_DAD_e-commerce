160674edd26507818c058db475d60d0622844f3f
Vincentfrg <vferrag78@gmail.com>
2024-08-29 14:45:49 +0200

    prepare API Route for Cart Managment

--- laravel-vue-ecommerce/routes/web.php
+++ laravel-vue-ecommerce/routes/web.php
@@ -1,15 +1,26 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ProductController::class, 'index'])->name('home');
-Route::get('/product/{product:slug}', [ProductController::class, 'view'])->name('product.view');
+Route::middleware(['guestOrVerified'])->group(function(){
+    Route::get('/', [ProductController::class, 'index'])->name('home');
+    Route::get('/product/{product:slug}', [ProductController::class, 'view'])->name('product.view');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    Route::prefix('/cart')-> name('cart.')->group(function(){
+        Route::get('/', [CartController::class, 'index'])->name('index');
+        Route::get('/add/{product:slug}', [CartController::class, 'add'])->name('add');
+        Route::get('/remove/{product:slug}', [CartController::class, 'remove'])->name('remove');
+        Route::get('/update-quantity/{product:slug}', [CartController::class, 'updateQuantity'])->name('update-quantity');
+    });
+});
+
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
